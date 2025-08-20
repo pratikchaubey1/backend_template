4677328e554e8a7cb70897a1847e580cf0866869
Pratik Chaubey <pratikup98@gmail.com>
2025-08-20 23:44:54 +0530

    GetTodo GetTodoid UpdateTodo

---
+++ controller/UpdateTodo.js
@@ -0,0 +1,29 @@
+const Todo = require('../models/Tool');
+
+const updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    // Return the updated document
+    const response = await Todo.findByIdAndUpdate(
+    {_id:id},
+      { title, description, updatedAt: Date.now() },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: 'Todo updated successfully',
+    });
+  } catch (e) {
+    console.error('error in updating data', e);
+    res.status(500).json({
+      success: false,
+      message: 'Error in updating data',
+    });
+  }
+};
+
+module.exports = updateTodo;
--- routes/todo.js
+++ routes/todo.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createTodo } = require('../controller/CreateTodo')
-const Gettodo = require('../controller/Gettodo')
-const GetTodo = require('../controller/GetTodoid')
+const { createTodo } = require('../controller/CreateTodo');
+const Gettodo = require('../controller/Gettodo');
+const GetTodo = require('../controller/GetTodoid');
+const UpdateTodo = require('../controller/UpdateTodo');
 
-router.post('/todo', createTodo)
-router.get('/Gettodo', Gettodo)
-router.get('/Gettodoid/:id', GetTodo)
-module.exports = router
+router.post('/todo', createTodo);
+router.get('/Gettodo', Gettodo);
+router.get('/Gettodoid/:id', GetTodo);
+router.put('/Updatetodo/:id', UpdateTodo);
+
+module.exports = router;
